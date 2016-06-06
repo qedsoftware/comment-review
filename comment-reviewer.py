47b1356f769f242586842b77e672e53fe1fcc968
William Wu <w@qed.ai>
2016-06-06 22:26:34 +0200

    print line numbers, do not print names of files with no comments, and ignore migration files

--- comment-reviewer.py
+++ comment-reviewer.py
@@ -12,23 +12,28 @@ repo = Repo (repo_path)
 files = repo.git.ls_tree('--name-only', r="master")
 file_list = files.split('\n')
 for fname in file_list:
-    if fname[-2:] != 'py':
+    if fname[-2:] != 'py' or "migration" in fname:
         continue
     file = open (os.path.join(repo_path,fname))
     content = file.read ()
     comments = []
     current_comment = ''
     flag = 0
+    line_num = 1
     for character in content:
         if character == '#':
             flag = 1
-        elif character == '\n' and flag:
-            flag = 0
-            comments.append(current_comment)
-            current_comment = ''
+            current_comment += "{}: ".format(line_num)
+        elif character == '\n': 
+            line_num += 1
+            if flag:
+                flag = 0
+                comments.append(current_comment)
+                current_comment = ''
         if flag:
             current_comment += character
-    print "--- File: %s ---" % fname
-    for comment in comments:
-        print comment
+    if len(comments) > 0:
+        print "--- File: %s ---" % fname
+        for comment in comments:
+            print comment
 
