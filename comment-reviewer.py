489cad7a545c9d413c1130f42813d153a01f81d6
William Wu <w@qed.ai>
2018-07-15 01:49:35 +0200

    refactoring: logger, argparse, python3, main, style

--- comment-reviewer.py
+++ comment-reviewer.py
@@ -1,47 +1,93 @@
-import os
-import sys
+#!/usr/bin/env python
 
+import os
+import argparse
+
+from pathlib import Path
 from git import Repo
 from enchant.checker import SpellChecker
 
-if len(sys.argv) != 2:
-    print "Usage: %s [path]" % sys.argv[0]
-    sys.exit()
 
-repo_path = sys.argv[1]
+class Logger:
 
-repo = Repo(repo_path)
-files = repo.git.ls_files()
-file_list = files.split('\n')
+    def __init__(self, color=True):
+        self.color = color
 
-spell_checker = SpellChecker("en_US")
+    def print_filename(self, s):
+        if self.color:
+            print(f"\033[34m\033[1m--- File: {s} ---\033[0m")
+        else:
+            print(f"--- File: {s} ---")
 
-for fname in file_list:
-    if fname[-3:] != '.py' or "migration" in fname:
-        continue
-    file = open(os.path.join(repo_path, fname))
-    content = file.read()
-    comments = []
-    current_comment = ''
-    flag = 0
-    line_num = 1
-    for character in content:
-        if character == '#':
-            flag = 1
-            current_comment += "\033[32m\033[1m{}: \033[0m".format(line_num)
-        elif character == '\n':
-            line_num += 1
-            if flag:
-                flag = 0
-                comments.append(current_comment)
-                current_comment = ''
-        if flag:
-            current_comment += character
-    if len(comments) > 0:
-        print "\033[34m\033[1m--- File: %s ---\033[0m" % fname
-        for comment in comments:
-            print comment
-            spell_checker.set_text(comment)
-            for err in spell_checker:
-                print "\033[31mERROR: %s\033[0m" % err.word
+    def print_comment(self, line_num, s):
+        if self.color:
+            print(f"\033[32m\033[1m{line_num}: \033[0m{s}")
+        else:
+            print(f"{line_num}: {s}")
 
+    def print_error(self, s):
+        if self.color:
+            print(f"\033[31mERROR: {s}\033[0m")
+        else:
+            print(f"ERROR: {s}")
+
+
+def parse_arguments():
+
+    parser = argparse.ArgumentParser(
+                 description='Inspect comments with possible spelling errors.')
+    parser.add_argument('--color', dest='color', action='store_true')
+    parser.add_argument('--no-color', dest='color', action='store_false')
+    parser.set_defaults(color=True)
+    parser.add_argument('--branch', type=str, default='master',
+                        help='branch of git repo to inspect; default is master')
+    parser.add_argument('repo_path', type=str,
+                        help='path to git repository to inspect')
+    return parser.parse_args()
+
+
+def main(color=True, repo_path='.', branch='master'):
+
+    logger = Logger(color=color)
+    spell_checker = SpellChecker("en_US")
+    repo = Repo(repo_path)
+    fnames = repo.git.ls_tree('--name-only', r=branch).split('\n')
+    fnames_py = [f for f in fnames
+                 if Path(f).suffix == '.py' and "migration" not in f]
+
+    for fname in fnames_py:
+
+        with open(os.path.join(repo_path, fname)) as f:
+            content = f.read()
+
+        comments = []
+        comment = ''
+        comment_line_num = 0
+        in_comment = False
+        line_num = 1
+
+        for ch in content:
+            if ch == '#':
+                in_comment = True
+                comment_line_num = line_num
+            elif ch == '\n':
+                line_num += 1
+                if in_comment:
+                    in_comment = False
+                    comments.append((comment_line_num, comment))
+                    comment = ''
+            if in_comment:
+                comment += ch
+
+        if len(comments) > 0:
+            logger.print_filename(fname)
+            for (line_num, comment) in comments:
+                logger.print_comment(line_num, comment)
+                spell_checker.set_text(comment)
+                for error in spell_checker:
+                    logger.print_error(error.word)
+
+
+if __name__ == "__main__":
+    args = parse_arguments()
+    main(**vars(args))
